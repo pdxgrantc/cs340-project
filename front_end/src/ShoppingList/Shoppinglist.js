910302a0c3b62fcf3a2560a11e1056f64365d66b
pdxgrantc <pdxgrantc@gmail.com>
2023-05-01 16:09:37 +0000

    clear list button

--- front_end/src/ShoppingList/Shoppinglist.js
+++ front_end/src/ShoppingList/Shoppinglist.js
@@ -33,7 +33,7 @@ export default function Shoppinglist() {
         return (
             <>
                 <Helmet>
-                    <title>Recipe</title>
+                    <title>Shopping List</title>
                 </Helmet>
                 <div className="bg-main_bg_color text-text_white min-h-[100vh] flex flex-col">
                     <Header />
@@ -72,33 +72,38 @@ function ListContent() {
     }
 
     return (
-        <div className='flex flex-col'>
-            <div className='flex gap-5'>
+        <div className='flex flex-col w-[80%]'>
+            <div className='flex gap-5 w-full justify-between'>
                 <h1 className='text-[3.75rem] font-semibold'>{user.displayName + "'s Shopping List"}</h1>
-                <button onClick={clearList}
-                    className="h-fit whitespace-nowrap text-[2rem] leading-8 cursor-pointer w-fit border-b-[1.5px] hover:bg-button_accent_color hover:ease-[cubic-bezier(0.4, 0, 1, 1)] duration-[350ms] hover:px-[1.5vw] py-[.25rem]">
-                    Clear List
-                </button>
+                {shoppingList.length !== 0 ?
+                    <button onClick={clearList}
+                        className="my-auto h-fit whitespace-nowrap text-[2rem] leading-8 cursor-pointer w-fit border-b-[1.5px] hover:bg-button_accent_color hover:ease-[cubic-bezier(0.4, 0, 1, 1)] duration-[350ms] hover:px-[1.5vw] py-[.25rem]">
+                        Clear List
+                    </button>
+                    :
+                    <></>
+                }
             </div>
-            <div className='flex flex-col'>
+            <div className='flex flex-col pl-5'>
                 {shoppingList.length !== 0 ?
                     <>
-                        {shoppingList.map((item) => (
+                        {shoppingList.map((item, index) => (
                             <div className='flex gap-10'>
                                 <div className='text-[2rem] flex gap-5'>
+                                    <p>{index + 1 + "."}</p>
                                     <p>{item.name}</p>
                                     <p>{item.amount}</p>
                                 </div>
                                 <Link
                                     to={'/recipe/' + item.recipe_id}
-                                    className="h-fit whitespace-nowrap text-[2rem] leading-8 cursor-pointer w-fit border-b-[1.5px] hover:bg-button_accent_color hover:ease-[cubic-bezier(0.4, 0, 1, 1)] duration-[350ms] hover:px-[1.5vw] py-[.25rem]">
+                                    className="align-middle h-fit whitespace-nowrap text-[2rem] leading-8 cursor-pointer w-fit border-b-[1.5px] hover:bg-button_accent_color hover:ease-[cubic-bezier(0.4, 0, 1, 1)] duration-[350ms] hover:px-[1.5vw] py-[.25rem]">
                                     Link to Recipe
                                 </Link>
                             </div>
                         ))}
                     </>
                     :
-                    <p>Shopping list is empty</p>
+                    <p className='text-[2rem]'>Shopping list is empty</p>
                 }
             </div>
         </div>
